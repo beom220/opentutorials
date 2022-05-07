0ef9fe8fc890548a66e8d3440be85bc5acab58d4
beom220 <beom220@gmail.com>
2022-05-07 14:09:29 +0900

    add create, but topics dosent refresh

--- reduxExample/js/store.js
+++ reduxExample/js/store.js
@@ -1,4 +1,4 @@
-/* FIXME
+/* NOTE
 *   Redux NPM import Error
 *   이유를 찾지 못하여서 CDN 통해 호출...
 *   바닐라형식으로 진행하느라 겪는 에러로 예상 */
@@ -11,16 +11,18 @@ const _contents = [
 
 function reducer(state, action){
 
+    // INITIALIZE
     if(state === undefined) {
         return  {
             max_id : 2,
-            mode : 'welcome',
+            mode : 'create',
             selcted_id : 2,
             contents : _contents,
         }
     }
 
     let newState = {};
+    // READ
     if(action.type === 'SELECT'){
         newState = Object.assign(
             {},
@@ -31,18 +33,24 @@ function reducer(state, action){
             }
         );
     }
+    // TODO CREATE ...
     if(action.type === 'CREATE'){
-
+        const newMaxId = state.max_id + 1;
+        const newContents = state.contents.concat();
+        console.log(newState);
+        newContents.push({id: newMaxId,title: action.title,desc: action.desc});
+        newState = Object.assign({},state,
+            {
+                max_id: newMaxId,
+                content: newContents,
+                mode: 'read'
+            }
+        );
     }
+    // TODO REMOVE ...
     if(action.type === 'REMOVE'){
-
+        console.log('actionType : remove');
     }
-    // console.log(`action : ${action}`);
-    // console.log(`state : ${state}`);
-    // console.log(`newState : ${newState}`);
-    // console.log(action);
-    // console.log(state);
-    // console.log(newState);
     console.log(newState);
     return newState;
 }
