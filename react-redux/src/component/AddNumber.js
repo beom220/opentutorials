fca76537debf7d4986d875bb0421f665018509cf
thbeom <beom220@gmail.com>
2022-05-13 21:04:52 +0900

    #3 update AddNumber with redux

--- react-redux/src/component/AddNumber.js
+++ react-redux/src/component/AddNumber.js
@@ -1,14 +1,16 @@
 import store from "../store";
+import {useState} from "react";
 
-export default function AddNumber(props) {
-    const onChange = (e) => props.onChange(Number(e.target.value));
+export default function AddNumber() {
+    const [number, setNumber] = useState(1);
+    const onChange = (e) => setNumber(Number(e.target.value));
     return (
         <div className="addNumber">
             <h1>Add Number</h1>
             <input type="button" value="+" onClick={()=>
-                store.dispatch({type:'INCREMENT', size: props.size})
+                store.dispatch({type:'INCREMENT', size: number})
             }/>
-            <input type="text" value={props.size} onChange={onChange}/>
+            <input type="text" value={number} onChange={onChange}/>
         </div>
     )
 }
