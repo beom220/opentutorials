b0ebf92ce2ee3db994264aeec8945298d3674f42
thbeom <beom220@gmail.com>
2022-05-13 20:59:32 +0900

    #3 update DisplayNumber with redux

--- react-redux/src/component/DisplayNumber.js
+++ react-redux/src/component/DisplayNumber.js
@@ -1,8 +1,16 @@
+import store from "../store";
+import {useState} from "react";
 export default function DisplayNumber(props){
+    const [state, setState] = useState({number : store.getState().number})
+
+    store.subscribe(()=>{
+        setState({number: store.getState().number})
+    })
+
     return (
         <div className="displayNumber">
             <h1>DisplayNumber</h1>
-            <input type="text" value={props.number} readOnly/>
+            <input type="text" value={state.number} readOnly/>
         </div>
     )
 }
