1942362ebbb20226434e97b2b523186332ae4a5f
thbeom <beom220@gmail.com>
2022-05-14 17:43:18 +0900

    Error 구현 #8

--- Node/main.js
+++ Node/main.js
@@ -4,23 +4,14 @@ const url = require('url');
 
 const app = http.createServer(function(request,response){
     let _url = request.url;
-    // Node.js에서 URL을 통해서 입력된 값을 사용하는 방법
-    const queryData = url.parse(_url, true).query;
+
+    const queryData = url.parse(_url, true).query; // Note Node.js에서 URL을 통해서 입력된 값을 사용하는 방법
+    const pathName = url.parse(_url, true).pathname;
     let title = queryData.id;
 
-    console.log(queryData);
-    if(_url == '/'){
-        title = 'WelCome'
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-
-    fs.readFile(`./data/${title}.txt`, 'utf-8', (err, data) => {
-        const templete = `
+    if(pathName === '/'){
+        fs.readFile(`./data/${title}.txt`, 'utf-8', (err, data) => {
+            const templete = `
             <!doctype html>
             <html>
                 <head>
@@ -38,7 +29,13 @@ const app = http.createServer(function(request,response){
                   <p>${data}</p>
                 </body>
             </html>`;
-        return response.end(templete);
-    })
+            response.writeHead(200);
+            response.end(templete);
+        })
+    } else {
+        response.writeHead(404); // Note 404 === page not found
+        response.end('Not found');
+    }
+
 });
 app.listen(3000);
