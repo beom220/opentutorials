1d77a0b2361332b9050651745d18a08779033695
thbeom <beom220@gmail.com>
2022-05-15 11:53:33 +0900

    기존 template 코드 모듈화 #8

--- Node/main.js
+++ Node/main.js
@@ -2,28 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
-
-const template = {
-    HTML: (title, list, body, control) => {
-        if(!control) control = '';
-        return `<!doctype html>
-            <html lang="ko">
-                <head>
-                  <title>${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body> 
-                  <h1><a href="/">WEB</a></h1>
-                  ${list}
-                  ${control}
-                  ${body}
-                </body>
-            </html>`;
-    },
-    list: (filelist) => {
-        return `<ul>${filelist?.map((file) => `<li><a href="/?id=${file}">${file}</a></li>`).join('')}</ul>`;
-    }
-}
+const template = require('./lib/template')
 
 const app = http.createServer((request,response) => {
     const _url = request.url;
