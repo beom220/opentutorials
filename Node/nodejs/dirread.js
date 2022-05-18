20b745f1f19f0c23e0bdac7c4a40fc85a4a69051
thbeom <beom220@gmail.com>
2022-05-19 00:33:43 +0900

    new project set

--- Node/nodejs/dirread.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const _dir = './data';
-
-fs.readdir(_dir, (err, file)=> console.log(file))
--- react-redux/src/component/DisplayNumber.js
+++
@@ -1,9 +0,0 @@
-export default function DisplayNumber(props){
-    return (
-        <div className="displayNumber">
-            <h1>DisplayNumber</h1>
-            <input type="text" value={props.number} readOnly/>
-            {props.unit}
-        </div>
-    )
-}
